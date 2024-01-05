f42dce6af93a98838bebd3158e1f684964176c68
Kris! <42949874+krstphrrr@users.noreply.github.com>
2024-01-05 16:21:23 -0700

    fixed

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+
+app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    // console.log(__dirname)
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+
 app.listen(8080);
 
